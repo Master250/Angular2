f9a4a6e8e7a3c7690bc498b35d5f322950600732
TIAMIYU <tiamiyusewou@hotmail.com>
2024-03-08 01:17:57 +0100

    Formulaire réactif

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,25 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Observable, map } from 'rxjs';
+import { FaceSnap } from '../models/face-snap-model';
+import { AsyncPipe, DatePipe, NgIf, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-new-face-snap',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf,UpperCasePipe,DatePipe, TitleCasePipe, AsyncPipe],
   templateUrl: './new-face-snap.component.html',
   styleUrl: './new-face-snap.component.scss',
 })
 export class NewFaceSnapComponent implements OnInit {
+  //Déclaration et création d'un observable
   snapForm!: FormGroup;
+  faceSnapPreview$!: Observable<FaceSnap>;
 
+  //Injection de formbuilder reactif
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    // construction de formulaire
     this.snapForm = this.formBuilder.group({
       title: [""],
       description: [""],
       imageUrl: [""],
       location: [""],
     });
+    //liaison de formulaire en faceSnaps
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map(formValue => ({
+        //Récupération tous les attributs du formulaire
+        ...formValue,
+        createdDate: new Date(),
+        snaps: 0,
+        id:0
+      }))
+    )
   }
 
   onSubmitForm() {
